eeff15c54938a6cf5ca27afcce44a5aa42ace1c9
nicolas <nicolasviruel@gmail.com>
2022-11-09 20:00:31 -0300

    Se corrigen errores en Json-server y warinings

--- src/App.js
+++ src/App.js
@@ -8,7 +8,29 @@ import ProductCreate from "./components/views/ProductCreate/ProductCreate";
 import ProductEdit from "./components/views/ProductEdit/ProductEdit";
 import Error404 from "./components/views/Error404/Error404";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { useEffect, useState } from "react";
+
 function App() {
+
+  const [products, setProducts] = useState([])
+  //Variable de entorno
+  const URL = process.env.REACT_APP_API_CAFETERIA;
+
+  useEffect(() => {
+    getApi();
+  }, [])
+  
+// recordar levantar Json server con el codigo: "json-server --watch db.json --port 4000" sino nunca trae los objetos creados
+  const getApi = async () => {
+    try {
+      const res = await fetch(URL);
+      const productApi = await res.json()
+      setProducts(productApi)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
 
@@ -17,7 +39,7 @@ function App() {
         <main>
           <Routes>
             <Route exact path="/" element={<Home/>} />
-            <Route exact path="/product/table" element={<ProductsTable/>}/>
+            <Route exact path="/product/table" element={<ProductsTable products={products} />}/>
             <Route exact path="/product/create" element={<ProductCreate/>}/>
             <Route exact path="/product/edit" element={<ProductEdit/>}/>
             <Route exact path="*" element={<Error404/>}/>
