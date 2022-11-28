412902852981b070ea047f11407c06fb9f0b02dc
nicolas <nicolasviruel@gmail.com>
2022-11-27 21:44:23 -0300

    se corrigen errores de una key faltante

--- src/components/views/ProductsTable/ProductsTable.js
+++ src/components/views/ProductsTable/ProductsTable.js
@@ -3,7 +3,7 @@ import { Container, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Product from "./Product/Product";
 
-const ProductTable = ({ products }) => {
+const ProductTable = ({ products , URL , getApi}) => {
   return (
     <div>
       <Container className="py-5">
@@ -18,7 +18,8 @@ const ProductTable = ({ products }) => {
         </div>
         <hr />
         {/* Table of products */}
-        <Table bordered hover responsive className="align-middle mt-3">
+        {products.length != 0 ?
+          <Table bordered hover responsive className="align-middle mt-3">
           <thead>
             <tr>
               <th>N.</th>
@@ -31,15 +32,22 @@ const ProductTable = ({ products }) => {
           </thead>
           <tbody>
             {products.map((product) => (
-              <Product key={product.id} product={product}/>
+              <Product key={product.id}
+               product={product}
+               URL={URL}
+               getApi= {getApi} />
               ))}
             
           </tbody>
         </Table>
-        {/* No products found message */}
-        {/* <div className="no-products-found d-flex align-items-center justify-content-center">
-          <h1>🥐 No products found ☕</h1>
-        </div> */}
+        :
+        <div className="no-products-found d-flex align-items-center justify-content-center">
+           //  {/* No products found message */}
+         <h1>🥐 No products found ☕</h1>
+       </div>
+      }
+      
+       
       </Container>
     </div>
   );
